c99bcabd4b122b83069e606361adaec91bf82286
Mario Martinez <amakenadog@gmail.com>
2017-04-25 16:37:49 -0600

    https://github.com/laravel/framework/issues/17508

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+// https://github.com/laravel/framework/issues/17508
+// https://github.com/laravel/docs/blob/5.4/migrations.md#index-lengths--mysql--mariadb
+use Illuminate\Support\Facades\Schema;
+
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // https://github.com/laravel/framework/issues/17508
+        // https://github.com/laravel/docs/blob/5.4/migrations.md#index-lengths--mysql--mariadb
+        Schema::defaultStringLength(191);
+
         app('view')->composer(['partials.nav', 'partials.menu', 'partials.projects.tab', 'user.tab', 'event.tab'], function($view) {
             $action = app('request')->route()->getAction();
             $controller = class_basename($action['controller']);
